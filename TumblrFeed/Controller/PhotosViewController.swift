9532115990c7d4279f99754e1e57a17dc76248b0
Mihai Ruber <>
2018-01-31 18:56:20 -0800

    finished basics

---
+++ TumblrFeed/Controller/PhotosViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  TumblrFeed
+//
+//  Created by Mihai Ruber on 1/31/18.
+//  Copyright © 2018 Mihai Ruber. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+import Alamofire
+
+class PhotosViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    // Variables
+    var posts: [[String:Any]] = []
+    
+    // Outlets
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view, typically from a nib.
+        // Network request snippet
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+
+                
+                // TODO: Get the posts and store in posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+                
+                print(self.posts)
+
+                
+                // TODO: Reload the table view
+                self.tableView.reloadData()
+            }
+        }
+        task.resume()
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        
+        let post = posts[indexPath.row]
+        
+        // Unwrap safely
+        if let photos = post["photos"] as? [[String: Any]] {
+            // photos is NOT nil, we can use it!
+            // TODO: Get the photo url
+            let photo = photos[0]
+            // 2.
+            let originalSize = photo["original_size"] as! [String: Any]
+            // 3.
+            let urlString = originalSize["url"] as! String
+            // 4.
+            let url = URL(string: urlString)
+            
+            //Set the image
+            cell.postImageView.af_setImage(withURL: url!)
+            
+        }
+        
+        return cell
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TumblrFeed/View/PhotoCell.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoCell.swift
+//  TumblrFeed
+//
+//  Created by Mihai Ruber on 1/31/18.
+//  Copyright © 2018 Mihai Ruber. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+    
+    // Variables
+    
+    // Outlets
+    
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
